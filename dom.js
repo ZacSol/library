5a0fc5dadd46f7e692b2952e302231f803bf4d47
ZacSol <42217556+ZacSol@users.noreply.github.com>
2018-09-24 10:56:09 -0500

    update

--- dom.js
+++ dom.js
@@ -142,6 +142,17 @@ function makeBox(object){
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
+
+function titleCase(string){
+    string=string.split(' ');
+    let newString="";
+    for(let i=0;i<string.length;i++){
+        newString+=`${sentenceCase(string[i])} `;
+    }
+    newString=newString.slice(0, -1);
+    return newString;
+}
+
 function dataGrab(inputId){
   const info=$(inputId).val();
   
